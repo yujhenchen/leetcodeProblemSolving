08ab5c71e9c4f5a27de6b401cf297c882eaa80c9
yujhenchen <yujhen.chen93@gmail.com>
2021-12-14 22:05:41 +0800

    longestCommonPrefix add simple solution

--- string/longestCommonPrefix.py
+++ string/longestCommonPrefix.py
@@ -1,21 +1,31 @@
+from typing import List
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         result = ""
 
-        add = True
-        stop = False
+        # My attempt
+        # add = True
+        # stop = False
+        # firstStr = strs[0]
+        # for i in range(len(firstStr)):
+        #     for j in range(len(strs)):
+        #         currentStr = strs[j]
+        #         if i > len(currentStr) - 1:
+        #             stop = True
+        #             break
+        #         if firstStr[i] != currentStr[i]:
+        #             add = False
+        #     if stop:
+        #         break
+        #     if add != False:
+        #         result += firstStr[i]
+
+        # Cool solution use zip, tuple, and set
         firstStr = strs[0]
-        for i in range(len(firstStr)):
-            for j in range(len(strs)):
-                currentStr = strs[j]
-                if i > len(currentStr) - 1:
-                    stop = True
-                    break
-                if firstStr[i] != currentStr[i]:
-                    add = False
-            if stop:
+        for charTuple in zip(*strs):
+            if len(set(charTuple)) == 1:
+                result += charTuple[0]
+            else:
                 break
-            if add != False:
-                result += firstStr[i]
 
         return result
